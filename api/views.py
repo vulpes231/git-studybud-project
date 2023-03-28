3df5b4737d6a7aad0a4ce660c87720edf5c7e262
Vulpes231 <whykayshot@gmail.com>
2023-03-28 08:14:21 +0100

    Created API for the app

---
+++ api/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+from django.http import JsonResponse
+
+
+def getRoutes(request):
+    routes = [
+        'GET /api',
+        'GET /api/rooms',
+        'GET /api/rooms/:id'
+    ]
+
+    return JsonResponse(routes, safe=False)
